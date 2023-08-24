abd17eb37eb61749d68d46af6b0e1718aa6edd71
james-freeburn <jfreeburn@swin.edu.au>
2023-08-24 14:56:58 +1000

    Made populations consistent with G13.

--- DWF_powerlaw_afterglows.py
+++ DWF_powerlaw_afterglows.py
@@ -1,7 +1,8 @@
 import numpy as np
+import os
 import afterglowpy as grb
 import pandas as pd
-from astropy.cosmology import Planck18 as cosmo
+from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
 import dask
 import dask.dataframe as dd
@@ -19,154 +20,204 @@ def GRBFR(z):
     e = (1+z)**1.7
     return R*e
 
-def GRB_spectrum(E):
+def GRB_spectrum(E,E_p):
     alpha = -1.0
     beta =  -2.3
     
-    E_0 = 250./(alpha-beta)
+    E_0 = E_p/(alpha+2.)
     if E < (alpha - beta)*E_0:
         return ((E/100.)**alpha)*np.exp(-E/E_0)
     else:
-        return ((alpha-beta)*E_0/100.)**(alpha-beta)*np.exp(beta-alpha)*(E/100.)**beta
+        return ((alpha-beta)*E_0/100.)**(alpha-beta)*np.exp(
+            beta-alpha)*(E/100.)**beta
 
-def GRB_parameters(rng,nevents):
-    # Generating GRB times and light curve coverage.
-    tlength = rng.integers(20,200,nevents)
-    tGRB = [rng.integers(-tlength_val + 3, 1440) for tlength_val in tlength]
-    
-    n0s = rng.uniform(0.1, 30.,nevents)
-    b_vals = rng.uniform(0.1,6.0,nevents)
-    ep_Bs = [0.008]*nevents
-    ep_es = [0.02]*nevents
-    ps = [2.5]*nevents
-    
-    # Use 5e50 as intrinsic energy of GRB
-    E_gamma = 1e50
-    
-    # Lorentz factor distribution
-    mu1,sigma1 = 4.525,1.475
-    Gamma_0s = np.exp(rng.normal(mu1,sigma1,nevents))
-    Beta_0s = (1+Gamma_0s**-2)**0.5
-    
-    # theta_j distribution
-    mu2,sigma2 = 1.742,0.916
-    theta_js = [0.]*nevents
-    for i in range(nevents):
-        theta_j = np.exp(rng.normal(mu2,sigma2))
-        while theta_j > 90.:
-            theta_j = np.exp(rng.normal(mu2,sigma2))
-        theta_js[i] = (np.pi/180.)*theta_j
-    theta_js = np.array(theta_js)
-    
-    # Isotropic energy is given by theta_j and Lorentz factor.
-    E_iso = np.array([E_gamma/(1-np.cos(theta_j)) if 1./Gamma_0 < np.sin(theta_j) 
-             else E_gamma*(1+Beta_0)*Gamma_0**2
-            for Gamma_0,Beta_0,theta_j in zip(Gamma_0s,Beta_0s,theta_js)])
-    
-    # Uniform distribution of theta_wing values (this is what we're testing)
-    theta_wings = [rng.uniform(theta_j,np.pi/2) for theta_j in theta_js]
-
-    # Generating probability density function for viewing angle.
-    theta = np.linspace(0.0, np.pi/2, 100000)
-    omega = 4*np.pi*np.sin(0.5*theta)**2
-    probability_density = np.array([((omega[i] - omega[i-1])/
-                                     (theta[i] - theta[i-1]))
-                                    for i in range(1,len(theta))])/omega[-1]
-    # Getting viewing angle values based on probability density.
-    thetaObs_vals = []
-    for i in range(nevents):
-        roll = 1.0
-        likelihood = probability_density[0]
-        while (roll > likelihood):
-            random_index = rng.integers(0,len(theta[theta < theta_wings[i]])-2)
-            val = theta[random_index]
-            likelihood = probability_density[random_index]
-            roll = rng.uniform(0.0,1.0)
-        thetaObs_vals.append(val)
-
-    # Placing GRBs at redshifts based on comoving volumes and SFH.
-    zs,d_Ls = GRB_redshifts(rng, nevents)
+def GRB_params(theta_js,Gamma_0s,E_iso,E_ps,zs,d_Ls,band,rng,nevents):
     
     # Truncated log-normal distribution for GRB duration.
-    upper,lower = np.log(1e5),np.log(2.0)
-    mu3, sigma3 = np.log(27.5), -np.log(0.35)
+    upper3,lower3 = np.log(1e5),np.log(2.0)
+    mu3, sigma3 = np.log(27.5),0.35
     T90 = np.exp(stats.truncnorm(
-            (lower - mu3) / sigma3, (upper - mu3) / sigma3, loc=mu3, scale=sigma3).rvs(nevents))
+            (lower3 - mu3)/sigma3, 
+            (upper3 - mu3)/sigma3, 
+            loc=mu3, 
+            scale=sigma3).rvs(
+        nevents,random_state=rng))
     
     # Calculating peak luminosity of GRB and bolometric flux of GRB
-    Lpeak = 2.*np.array(E_iso)*(1.+np.array(zs))/T90
+    Lpeak = 2.*E_iso*(1.+np.array(zs))/T90
     Pbol = Lpeak/(4*np.pi*d_Ls**2)
+    Fbol = E_iso*(1.+np.array(zs))/(4*np.pi*d_Ls**2)
 
     # GRB Spectrum
-    Es = np.geomspace(1.0,int(1e4),int(1e5)) #keV
-    spectrum = np.array([GRB_spectrum(E) for E in Es])
-    
-    # For re-normalising the spectrum for integration  
-    P_peak = np.trapz(spectrum,Es)
-    
-    # Integrate across Swift's wavelength range to get a flux (P).
-    range_min,range_max = find_nearest(Es,15.),find_nearest(Es,150.)
-    P = (Pbol/P_peak)*np.trapz(spectrum[range_min:range_max],Es[range_min:range_max])
-    
-    # Convert flux to a photon flux.
-    photon_flux = np.array([np.mean(flux/(u.keV.to(u.erg)*Es[range_min:range_max])) for flux in P])
+    Es = np.geomspace(1e0,1e4,int(1e3)) #keV
+    #spectrum = np.array([GRB_spectrum(E,100.) for E in Es])
+    normalisation_factor = np.array([0.]*len(E_ps))
+    integrations = np.array([0.]*len(E_ps))
+    photon_integrations = np.array([0.]*len(E_ps))
+    for i,E_p in enumerate(E_ps):
+        spectrum = np.array([GRB_spectrum(E,E_p) for E in Es])
+        SwiftRange = [find_nearest(Es,band[0]*(1+zs[i])),
+                      find_nearest(Es,band[1]*(1+zs[i]))]
+        # Integrate across Swift's wavelength range to get a flux and fluence.
+        normalisation_factor[i] = np.trapz(spectrum*u.keV.to(u.erg)*Es,
+                                           u.keV.to(u.erg)*Es)
+        integrations[i] = np.trapz(spectrum[SwiftRange[0]:SwiftRange[1]]
+                                   *u.keV.to(u.erg)*
+                                   Es[SwiftRange[0]:SwiftRange[1]],
+                                   u.keV.to(u.erg)*
+                                   Es[SwiftRange[0]:SwiftRange[1]])
+        # Convert flux to a photon flux.
+        photon_integrations[i] = np.trapz(spectrum[SwiftRange[0]:
+                                                   SwiftRange[1]],
+                                          u.keV.to(u.erg)*
+                                          Es[SwiftRange[0]:SwiftRange[1]])
+        
+    P = (Pbol/normalisation_factor)*integrations
+    F = (Fbol/normalisation_factor)*integrations
+    photon_flux = (Pbol/normalisation_factor)*photon_integrations
     # If photon flux is above 2.6, we get a Swift detection.
     Swift_GRB = photon_flux > 2.6
     
-    return pd.DataFrame(np.transpose([thetaObs_vals,
-                                      theta_wings,
-                                      theta_js,
-                                      b_vals,
-                                      zs,
-                                      d_Ls,
-                                      n0s,
-                                      ps,
-                                      ep_Bs,
-                                      ep_es,
-                                      E_iso,
-                                      tlength,
-                                      tGRB,
-                                      Gamma_0s,
-                                      T90,
-                                      P,
-                                      photon_flux,
-                                      Swift_GRB]),
-                        columns=['theta_v',
-                                 'theta_w',
-                                 'theta_j',
-                                 'b',
-                                 'z',
-                                 'd_L',
-                                 'n0',
-                                 'p',
-                                 'epsilon_B',
-                                 'epsilon_e',
-                                 'Eiso',
-                                 'tlength',
-                                 'tGRB',
-                                 'Gamma_0',
-                                 'T90',
-                                 'GRB_flux',
-                                 'GRB_photon_flux',
-                                 'Swift_GRB'])
+    return T90,F,P,photon_flux,Swift_GRB
 
-def GRB_redshifts(rng,nevents):
-    redshifts = np.linspace(0.01,10.0,100000)
-    SFH = GRBFR(redshifts)
+def population_synthesis(rng,nevents,cosmo):
+    
+    # Intrinsic energy of GRBs
+    E_gamma_dash = 1.5e48
+    E_p_dash = 1.5
+    
+    # Lorentz factor distribution
+    mu1,sigma1 = 4.525,1.475
+    mu2,sigma2 = 1.742,0.916
+
+    # The desired covariance matrix.
+    cov = np.array([
+            [sigma2, -.87],
+            [-.87, sigma1]
+        ])
+
+    L = np.linalg.cholesky(cov)
+
+    uncorrelated = [np.log(stats.lognorm.rvs(sigma2, 
+                                             scale=np.exp(mu2), 
+                                             size=nevents)),
+                    np.log(stats.lognorm.rvs(sigma1, 
+                                             scale=np.exp(mu1), 
+                                             size=nevents))]
+    correlated = np.dot(L, uncorrelated)
+
+    uncorr_mean = [np.mean(uncorrelated[0]), np.mean(uncorrelated[1])]
+    corr_mean = [np.mean(correlated[0]), np.mean(correlated[1])]
+    correlated = correlated - np.array(
+        corr_mean).reshape(2, 1) + np.array(
+            uncorr_mean).reshape(2, 1)
+
+    X, Y = correlated
+    
+    Gamma_0s = np.exp(Y)
+    theta_js = np.exp(X)*np.pi/180.
+    
+    mask = (theta_js < np.pi/2.) & (Gamma_0s < 8e3) & (Gamma_0s > 1.)
+    
+    Gamma_0s = Gamma_0s[mask]
+    theta_js = theta_js[mask]
+    
+    df = pd.DataFrame([])
+    df = df.assign(Gamma_0=Gamma_0s,
+                   theta_j=theta_js)
+    tlength = rng.integers(20,200,len(df))
+    tGRB = [rng.integers(-tlength_val + 3, 1440) 
+                          for tlength_val in tlength]    
+    df = df.assign(Beta_0 = (1.-(1./df.Gamma_0**2.))**0.5,
+                   E_gamma = E_gamma_dash*df.Gamma_0,
+                   tlength = tlength,
+                   tGRB = tGRB,
+                   n0 = rng.uniform(0.1,30.,len(df)),
+                   b = rng.uniform(0.0,3.0,len(df)),
+                   epsilon_B = [0.008]*len(df),
+                   epsilon_e = [0.02]*len(df),
+                   p = [2.3]*len(df),
+                   theta_w = [rng.uniform(theta_j,np.pi/2) 
+                              for theta_j in df.theta_j])
+    
+    # Generating probability density function for viewing angle.
+    theta = np.linspace(0.0, np.pi/2, 100000)
+    probability_density = np.sin(theta)
+    probability_density = probability_density/np.sum(probability_density)
+    
+    df = df.assign(theta_v=[rng.choice(theta, p=probability_density) 
+                            for theta_w in df.theta_w],
+                   E_p = E_p_dash*5.*df.Gamma_0/(5.-2.*df.Beta_0),
+                   Eiso = [E_gamma/(1-np.cos(theta_j)) 
+                           if 1./Gamma_0 < np.sin(theta_j) 
+                           else E_gamma*(1+Beta_0)*Gamma_0**2
+                           for E_gamma,Gamma_0,Beta_0,theta_j 
+                           in zip(df.E_gamma,df.Gamma_0,df.Beta_0,df.theta_j)])
+    
+    # Placing GRBs at redshifts based on comoving volumes and SFH.
+    zs,d_Ls = GRB_redshifts(rng, len(df), cosmo)
+    df = df.assign(z=zs,d_L=d_Ls)
+
+    # Uniform distribution of theta_wing values (this is what we're testing)
+    
+    PO_mask = (df.theta_v < df.theta_j) | (np.sin(df.theta_v) < 1./df.Gamma_0)
+    
+    PO_events = df[PO_mask]
+    NPO_events = df[PO_mask == False]
+    
+    T90,F,P,photon_flux,Swift_GRB = GRB_params(PO_events.theta_j,
+                                               PO_events.Gamma_0,
+                                               PO_events.Eiso,
+                                               PO_events.E_p,
+                                               PO_events.z,
+                                               PO_events.d_L,
+                                               [15.,150.],
+                                               rng,len(PO_events))
+    
+    PO_events = PO_events.assign(T90=T90,
+                                 GRB_fluence=F,
+                                 GRB_flux=P,
+                                 GRB_photon_flux=photon_flux,
+                                 Swift_GRB=Swift_GRB)
+    
+    NPO_events = NPO_events.assign(T90=np.nan,
+                                   GRB_fluence=np.nan,
+                                   GRB_flux=np.nan,
+                                   GRB_photon_flux=np.nan,
+                                   Swift_GRB=False)
+    
+    df = pd.concat([PO_events,NPO_events]).reset_index(drop=True)
+    
+    return df[['theta_v',
+              'theta_w',
+              'theta_j',
+              'b',
+              'z',
+              'd_L',
+              'n0',
+              'p',
+              'epsilon_B',
+              'epsilon_e',
+              'Eiso',
+              'tlength',
+              'tGRB',
+              'Gamma_0',
+              'E_p',
+              'T90',
+              'GRB_fluence',
+              'GRB_flux',
+              'GRB_photon_flux',
+              'Swift_GRB']]
+
+def GRB_redshifts(rng,nevents,cosmo):
+    redshifts = np.linspace(0.01,10.0,1000)
     V = cosmo.comoving_volume(redshifts)
     volume_element = np.gradient(V,redshifts)
-    probability = SFH*volume_element
-    probability_density = probability/np.trapz(probability,redshifts)
-    zs = []
-    for i in range(nevents):
-        roll = 1.0
-        likelihood = probability_density[0]
-        while (roll > likelihood):
-            random_index = rng.integers(0,len(redshifts)-2)
-            z = redshifts[random_index]
-            likelihood = probability_density[random_index]
-            roll = rng.uniform(0.0,1.0)
-        zs.append(z)
+    
+    probability = GRBFR(redshifts)*volume_element.value/(1+redshifts)
+    probability_density = probability/np.sum(probability)
+    
+    zs = rng.choice(redshifts, nevents, p=probability_density)
     d_Ls = cosmo.luminosity_distance(np.array(zs)).to(u.cm).value
     return zs,d_Ls
 
@@ -174,7 +225,7 @@ def check_detectability(row):
     d = {}
 
     # For convenience, place arguments into a dict.
-    Z = {'jetType':     grb.jet.PowerLaw,     # Power Law with Core
+    Z = {'jetType':     grb.jet.PowerLawCore,     # Power Law with Core
          'specType':    0,                  # Basic Synchrotron Spectrum
          'thetaObs':    row.theta_v,   # Viewing angle in radians
          'E0':          row.Eiso, # Isotropic-equivalent energy in erg
@@ -214,15 +265,21 @@ def check_detectability(row):
 if __name__ == "__main__":
 
     rng = np.random.default_rng(seed=12345)
-    nevents = 1000000
+    nevents = 10000000
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.3, Tcmb0=2.725)
     out = 'GRB_afterglows_powerlaw_new.csv'
 
-    # Generating GRB parameters.
-    print('Generating Parameters for',nevents,'events.')
-    meta_data = GRB_parameters(rng,nevents)
-    print('done!')
+    if os.path.exists(out):
+        GRB_population = pd.read_csv(out)
+    else:
+        # Generating GRB parameters.
+        print('Generating ',nevents,'GRB events.')
+        GRB_population = population_synthesis(rng,nevents,cosmo)
+        GRB_population.Swift_GRB = GRB_population.Swift_GRB.astype(bool)
+        GRB_population.to_csv(out,index=False)
+        print('done!')
 
-    dask.config.set({'distributed.scheduler.allowed-failures': 100})
+    dask.config.set({'distributed.scheduler.allowed-failures': 500})
     cluster = SLURMCluster(cores=1,
                        processes=1,
                        memory="800MB",
@@ -233,11 +290,13 @@ if __name__ == "__main__":
                        )
     cluster.adapt(minimum=1,maximum=500)
     client = Client(cluster)
-    meta_data_dd = dd.from_pandas(meta_data,npartitions=1000)
-    detectable_arr = meta_data_dd.apply(check_detectability,axis=1,
+    GRB_population_dd = dd.from_pandas(GRB_population,npartitions=1000)
+    detectable_arr = GRB_population_dd.apply(check_detectability,axis=1,
                                     meta={'detectable':bool,
                                           'peak_mag':float}).compute()
-    meta_data = meta_data.assign(DWF_afterglow = detectable_arr.detectable,
-                                 afterglow_peakmag = detectable_arr.peak_mag)
-    meta_data.to_csv(out)
+    detectable_arr.to_csv(out.replace('.csv','_detectability.csv'))
+    GRB_population = GRB_population.assign(
+        DWF_afterglow = detectable_arr.detectable,
+        afterglow_peakmag = detectable_arr.peak_mag)
+    GRB_population.to_csv(out,index=False)
         
